2baad392fc80882e8ea229f241b499b3c526eb86
iurig21 <a32192@alunos.ipca.pt>
2026-02-24 10:37:02 +0000

    deploy configuration

--- backend/db.js
+++ backend/db.js
@@ -5,6 +5,7 @@ const config = {
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_DATABASE,
+  port: parseInt(process.env.DB_PORT || '1433', 10),
   options: {
     encrypt: true,
     trustServerCertificate: true,
